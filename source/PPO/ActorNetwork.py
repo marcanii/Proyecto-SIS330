9093ac3431b284c804c4073f25ba30be26934527
marcanii <israelmarcani10@gmail.com>
2024-04-28 23:12:43 -0400

    implementando yolo en formato onnx, para la jetson nano

--- source/PPO/ActorNetwork.py
+++ source/PPO/ActorNetwork.py
@@ -5,11 +5,27 @@ import torch.nn as nn
 import torchvision.models as models
 from torch.distributions.categorical import Categorical 
 
+# Define una capa convolucional personalizada para aceptar imágenes en blanco y negro
+class CustomConv2d(nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                         padding=0, dilation=1, groups=1, bias=True):
+        super(CustomConv2d, self).__init__(
+                    in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        
+        def forward(self, x):
+                # Asumiendo que la entrada es una sola capa (escala de grises)
+            if x.dim() == 3:
+                x = x.unsqueeze(1)
+            return super(CustomConv2d, self).forward(x)
+
 class ActorNetwork(nn.Module):
     def __init__(self, n_outputs, alpha, pretrained=False, freeze=False, chkpt_dir='tmp/ppo'):
         super(ActorNetwork, self).__init__()
-        
+        # Reemplaza la primera capa convolucional de ResNet con la capa personalizada
         resnet50 = models.resnet50(weights=None if not pretrained else 'imagenet')
+        resnet50.conv1 = CustomConv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        
+        #resnet50 = models.resnet50(weights=None if not pretrained else 'imagenet')
         self.resnet50 = nn.Sequential(*list(resnet50.children())[:-1])
         if freeze:
             for param in self.resnet50.parameters():
--- source/main.py
+++ source/main.py
@@ -2,36 +2,59 @@ import cv2
 import torch
 import numpy as np
 from PPO.Environment import Environment
-from Yolo.yolo_seg import YoloSegmentation
+from Yolo.yolo_seg1 import YOLOSeg
 from PPO.Agent import Agent
 from CAE.maxPooling import MaxPooling
 import matplotlib.pyplot as plt
 
+def convert_to_grayscale(image):
+    # Tomar la media de los dos canales para cada píxel
+    grayscale_image = np.mean(image, axis=0, keepdims=True)
+    return grayscale_image
+
+def convert_to_two_channels(image):
+    # Reorganizar los ejes para tener el formato (altura, ancho, canales)
+    image_reordered = np.transpose(image, (1, 2, 0))
+    # Seleccionar solo los dos primeros canales
+    two_channel_image = image_reordered[:, :, :2]
+    return two_channel_image
+
 if __name__ == '__main__':
     env = Environment()
-    modelSegmentation = YoloSegmentation()
+    modelSegmentation = YOLOSeg("source/Yolo/runs/segment/train3/weights/best.onnx")
     maxPooling = MaxPooling()
-    agent = Agent(5, 3*60*108) # webcam with 3*64*108 
+    agent = Agent(5, 1*60*108) # webcam with 3*64*108 
 
     #img = env.observation()
-    img = cv2.imread("F:\Proyecto-SIS330\source\\2.jpg")
+    img = cv2.imread("source/2.jpg")
     print("InputImage: ", img.shape)
-    seg_image = modelSegmentation.segment_image(img)
+    seg_image = modelSegmentation(img)
+    print("SegImage: ", type(seg_image))
+    seg_image = convert_to_two_channels(seg_image)
+    seg_image = convert_to_grayscale(seg_image)
     print("SegImage: ", seg_image.shape)
-    seg_image_torch = np.transpose(seg_image, (2, 0, 1))
-    seg_image_torch = torch.from_numpy(seg_image_torch).unsqueeze(0)
+    seg_image_torch = torch.from_numpy(seg_image).unsqueeze(0)
     print("SegImageTorch: ", seg_image_torch.shape)
     inputImgPOO = maxPooling(seg_image_torch)
     print("InputImagePOO: ", inputImgPOO.shape)
+    inputImgPOO = inputImgPOO.to(torch.float32)
     action, probs, value = agent.choose_action(inputImgPOO)
     print("Action: ", action)
     print("Probs: ", probs)
     print("Value: ", value)
 
+    seg_image = (seg_image * 255).astype(np.uint8).transpose(1, 2, 0)
+    inputImgPOO=inputImgPOO.squeeze(0).permute(1, 2, 0).detach().numpy()
+
     while True:
+        print("----------------------------------------------")
+        print("InputImage: ", img.shape)
         cv2.imshow("Image Input", img)
+        #seg_image = (seg_image * 255).astype(np.uint8).transpose(1, 2, 0)
+        print("SegImage: ", seg_image.shape)
         cv2.imshow("Mask", seg_image)
-        cv2.imshow("InputImagePOO", inputImgPOO.squeeze(0).permute(1, 2, 0).detach().numpy())
+        print("InputImagePOO: ", inputImgPOO.shape)
+        cv2.imshow("InputImagePOO", inputImgPOO)
         if cv2.waitKey(1) & 0xFF == ord('q'):  # Presiona 'q' para salir
             break
 
