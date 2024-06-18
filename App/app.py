51510b09311b6269a05887faa245ba50a911f6ef
marcanii <israelmarcani10@gmail.com>
2024-06-18 14:21:13 -0400

    modificando app para presentar de tiempo real

--- App/app.py
+++ App/app.py
@@ -43,10 +43,10 @@ def getVideo(canvas, image_label):
 def send_instruction(instruction):
     cliente_socket.send(str(instruction).encode())
 
-def move_forward():
+def move_backward():
     send_instruction(1)
 
-def move_backward():
+def move_forward():
     send_instruction(2)
 
 def move_left():
@@ -73,6 +73,7 @@ def set_mode():
         enable_controls()
     else:
         disable_controls()
+        send_instruction(10)
 
 def enable_controls():
     forward_button.config(state=tk.NORMAL)
